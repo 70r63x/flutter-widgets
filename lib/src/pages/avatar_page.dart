a47b6bf97e7af28efd8402ce8c11e77840b029a9
Jorge Herrera <jorgearturo.herrerajimenez@gmail.com>
2021-04-07 23:56:33 -0500

    crear avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,24 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
-      )
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Icon(Icons.person, color: Colors.red,),
+              //backgroundColor: Colors.brown,
+              //foregroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/9/91/Oahu_Landscape.jpg'),
+              backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/9/91/Oahu_Landscape.jpg'),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/9/91/Oahu_Landscape.jpg'),
+        ),
+      ),
     );
   }
 }
