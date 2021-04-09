31a3a79f9baec3695e2353635b050982d65a2c48
Jorge Herrera <jorgearturo.herrerajimenez@gmail.com>
2021-04-08 20:53:12 -0500

    crear animaciones

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_widgets/src/pages/alert_page.dart';
+import 'package:flutter_widgets/src/pages/animated_container.dart';
 import 'package:flutter_widgets/src/pages/avatar_page.dart';
 import 'package:flutter_widgets/src/pages/card_page.dart';
 // import 'package:flutter_widgets/src/pages/home_temp.dart';
@@ -13,5 +14,6 @@ Map<String, WidgetBuilder> getAplicationRoutes(){
     'alert' : (BuildContext context) => AlertPage(),
     'avatar' : (BuildContext context) => AvatarPage(),
     'card' : (BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
   };
 }
